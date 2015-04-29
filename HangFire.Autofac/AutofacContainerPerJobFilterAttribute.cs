f19217322542624d85559c3f09f10c804ec8c6fc
SevenSpikes <sevenspikes@gmail.com>
2015-04-29 11:15:47 +0300

    Implemented job activation per lifetime scope
    Hangfire does not support per request containers. This means that all
    dependencies will be shared between jobs, which is far from ideal,
    especially when you have IDbContext. This is on the road map of Hangfire
    2.0.
    We need a work around this. This is why we add our own
    AutofacJobActivator and a JobFilter to manage a per job lifetime scope.

---
+++ HangFire.Autofac/AutofacContainerPerJobFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Hangfire.Common;
+using Hangfire.Server;
+
+namespace Hangfire
+{
+    public class AutofacContainerPerJobFilterAttribute : JobFilterAttribute, IServerFilter
+    {
+        private readonly AutofacPerLifetimeScopeJobActivator _autofacPerLifetimeScopeJobActivator;
+
+        public AutofacContainerPerJobFilterAttribute(AutofacPerLifetimeScopeJobActivator autofacPerLifetimeScopeJobActivator)
+        {
+            _autofacPerLifetimeScopeJobActivator = autofacPerLifetimeScopeJobActivator;
+        }
+
+        public void OnPerformed(PerformedContext filterContext)
+        {
+            _autofacPerLifetimeScopeJobActivator.DisposeJobLifetimeScope();
+        }
+
+        public void OnPerforming(PerformingContext filterContext)
+        {
+            _autofacPerLifetimeScopeJobActivator.CreateJobLifetimeScope();
+        }
+    }
+}
---
+++ HangFire.Autofac/HangfirePerLifetimeScopeConfigurer.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+
+namespace Hangfire
+{
+    public static class HangfirePerLifetimeScopeConfigurer
+    {
+        public static void Configure(IContainer container)
+        {
+            var autofacPerLifetimeScopeJobActivator = new AutofacPerLifetimeScopeJobActivator(container);
+            JobActivator.Current = autofacPerLifetimeScopeJobActivator;
+            GlobalJobFilters.Filters.Add(new AutofacContainerPerJobFilterAttribute(autofacPerLifetimeScopeJobActivator));
+        }
+    }
+}
