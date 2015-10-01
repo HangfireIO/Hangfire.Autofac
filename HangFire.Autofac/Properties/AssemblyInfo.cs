b438c19db8c5c104bed1e1005db2fefba924d22f
Sergey Odinokov <odinserj@gmail.com>
2015-10-01 19:36:42 +0300

    Bump version to 1.2.0

--- HangFire.Autofac/Properties/AssemblyInfo.cs
+++ HangFire.Autofac/Properties/AssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c38d8acf-7b2c-4d7f-84ad-c477879ade3f")]
 
-[assembly: AssemblyInformationalVersion("1.2.0-beta2")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
 [assembly: AssemblyVersion("1.2.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
