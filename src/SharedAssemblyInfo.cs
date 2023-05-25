2c2874fef7f321a7d72d5b788d38ed664a7be44b
Sergey Odinokov <odinserj@gmail.com>
2023-05-25 16:56:18 +0700

    Update copyrights

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Hangfire")]
-[assembly: AssemblyCompany("Sergey Odinokov")]
-[assembly: AssemblyCopyright("Copyright © 2014 Sergey Odinokov")]
+[assembly: AssemblyCompany("Hangfire OÜ")]
+[assembly: AssemblyCopyright("Copyright © 2014-2023 Hangfire OÜ")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
