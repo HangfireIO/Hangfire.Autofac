318e52535f9ea697dfde12efd6c21005d5e7cf3a
Sergey Odinokov <odinserj@hangfire.io>
2024-02-16 10:45:45 +0700

    Re-add some assembly metadata

---
+++ src/Hangfire.Autofac/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hangfire.Autofac")]
+[assembly: AssemblyDescription("Autofac IoC Container integration support for Hangfire.")]
+[assembly: Guid("c38d8acf-7b2c-4d7f-84ad-c477879ade3f")]
