3b7e357110f5b8dde9cf64eabba50d6c5984a2fc
Sergey Odinokov <odinserj@hangfire.io>
2024-09-24 15:43:18 +0700

    Update GlobalConfigurationExtensionsFacts.cs

--- tests/Hangfire.Autofac.Tests/GlobalConfigurationExtensionsFacts.cs
+++ tests/Hangfire.Autofac.Tests/GlobalConfigurationExtensionsFacts.cs
@@ -38,5 +38,40 @@ namespace Hangfire.Autofac.Tests
 
             Assert.IsType<AutofacJobActivator>(JobActivator.Current);
         }
+
+#if !NET452
+        [Fact]
+        public void UseAutofacActivator_WithConfigurationAction_ThrowsAnException_WhenConfigurationIsNull()
+        {
+            var lifetimeScope = new Moq.Mock<ILifetimeScope>();
+
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => ((IGlobalConfiguration)null).UseAutofacActivator(lifetimeScope.Object, (_, _) => {  }));
+
+            Assert.Equal("configuration", exception.ParamName);
+        }
+
+        [Fact]
+        public void UseAutofacActivator_WithConfigurationAction_ThrowsAnException_WhenLifetimeScopeIsNull()
+        {
+            var configuration = new Moq.Mock<IGlobalConfiguration>();
+
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => configuration.Object.UseAutofacActivator(null, (_, _) => {  }));
+
+            Assert.Equal("lifetimeScope", exception.ParamName);
+        }
+
+        [Fact]
+        public void UseAutofacActivator_WithConfigurationAction_CallsUseActivatorCorrectly()
+        {
+            var configuration = new Moq.Mock<IGlobalConfiguration>();
+            var lifetimeScope = new Moq.Mock<ILifetimeScope>();
+
+            configuration.Object.UseAutofacActivator(lifetimeScope.Object, (_, _) => {  });
+
+            Assert.IsType<AutofacJobActivator>(JobActivator.Current);
+        }
+#endif
     }
 }
