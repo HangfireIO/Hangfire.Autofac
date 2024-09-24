382b6203c198c4451dda7a8a32bd48586230b5b6
Sergey Odinokov <odinserj@hangfire.io>
2024-09-24 11:25:13 +0700

    Create GlobalConfigurationExtensionsFacts.cs

---
+++ tests/Hangfire.Autofac.Tests/GlobalConfigurationExtensionsFacts.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Autofac;
+using Xunit;
+
+namespace Hangfire.Autofac.Tests
+{
+    public class GlobalConfigurationExtensionsFacts
+    {
+        [Fact]
+        public void UseAutofacActivator_ThrowsAnException_WhenConfigurationIsNull()
+        {
+            var lifetimeScope = new Moq.Mock<ILifetimeScope>();
+
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => ((IGlobalConfiguration)null).UseAutofacActivator(lifetimeScope.Object));
+
+            Assert.Equal("configuration", exception.ParamName);
+        }
+
+        [Fact]
+        public void UseAutofacActivator_ThrowsAnException_WhenLifetimeScopeIsNull()
+        {
+            var configuration = new Moq.Mock<IGlobalConfiguration>();
+
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => configuration.Object.UseAutofacActivator(null));
+
+            Assert.Equal("lifetimeScope", exception.ParamName);
+        }
+
+        [Fact]
+        public void UseAutofacActivator_CallsUseActivatorCorrectly()
+        {
+            var configuration = new Moq.Mock<IGlobalConfiguration>();
+            var lifetimeScope = new Moq.Mock<ILifetimeScope>();
+
+            configuration.Object.UseAutofacActivator(lifetimeScope.Object);
+
+            Assert.IsType<AutofacJobActivator>(JobActivator.Current);
+        }
+    }
+}
